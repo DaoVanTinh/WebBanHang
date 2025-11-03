61016de93794e9580019758ee4215792028f74f0
Dao Van Tinh <tinhdao9@gmail.com>
2025-11-03 20:11:41 +0700

    Cập nhật models người dùng

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/Webbanhang");
 const app = express();
 
 app.listen(8080, () => {
-    console.log('Server is running!');
+  console.log("Server is running!");
 });
---
+++ server/models/users.js
@@ -0,0 +1,19 @@
+import Password from "antd/es/input/Password";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: Number, required: true },
+    address: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("User", userSchema);
